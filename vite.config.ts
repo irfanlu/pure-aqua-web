523cf1339297df9065a2cedb258bdfa704f0a21f
Mohd Irfan <irfanjspslnpw@gmail.com>
2026-03-11 15:51:38 +0530

    Update Vite config base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "./" : "/",
+export default defineConfig({
+  base: "/pure-aqua-web/",
   server: {
     host: "::",
     port: 8080,
@@ -18,4 +17,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
